53b2fd13acc2a7f7fcf608ff2b6f48a7b0cea0ad
bigaznee666 <viktor.tellstig@rosendalsgymnasiet.se>
2022-10-06 14:06:07 +0200

    jesse

--- src/Testklass.java
+++ src/Testklass.java
@@ -2,6 +2,7 @@ public class Testklass {
     public static void main(String[] args) {
         System.out.println("My name is Skyler White, yo");
         System.out.println("My husband is Walter White, yo");
+        System.out.println("TIHI jag är viktor tlelstiksag och ajg bog");
     }
     
 }
