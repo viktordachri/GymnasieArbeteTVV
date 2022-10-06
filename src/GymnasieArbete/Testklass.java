808324e9928e634ed357cb8aac58c2739c0ac527
bigaznee666 <viktor.tellstig@rosendalsgymnasiet.se>
2022-10-06 14:11:20 +0200

    jesser

--- src/GymnasieArbete/Testklass.java
+++ src/GymnasieArbete/Testklass.java
@@ -1,8 +1,12 @@
 package GymnasieArbete;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Testklass {
     public static void main(String[] args) {
         System.out.println("AHAHAHAHAHHAHA jag är en ond clown!!!");
+        System.out.println("ayo mista white");
     }
+
 
 }
